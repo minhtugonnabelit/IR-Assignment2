22671e5e64e2a9c90fea6c130ef1bb81555d0cda
anhminhtu <anhminh.tu@student.uts.edu.au>
2023-10-05 16:05:21 +1100

    daily update

--- gui.py
+++ gui.py
@@ -24,15 +24,13 @@ class RobotGUI:
 
         self.sawyer = Sawyer(self.env, base=sm.SE3(0, 1, 1))
         self.sawyer_controller = Controller(self.sawyer, self.env)
-        self.sawyer_qlim = copy.deepcopy(self.sawyer.qlim)
-        self.sawyer_qlim = np.rad2deg(self.sawyer_qlim)
+        self.sawyer_qlim = np.rad2deg(copy.deepcopy(self.sawyer.qlim))
         self.sawyer_controller.go_to_home()
-
 
         self.astorino = Astorino(self.env, base=sm.SE3(1, 0, 1))
         self.astorino_controller = Controller(self.astorino, self.env)
+        self.astorino_qlim = np.rad2deg(copy.deepcopy(self.astorino.qlim))
         self.astorino_controller.go_to_home()
-
 
         self.window = self.create_window()
         self.update_gui_thread()
@@ -115,48 +113,47 @@ class RobotGUI:
     def tab2_setup(self):
         # Define the layout for the first tab
         tab2_layout = [
-            # [
+            [
             [sg.Text('Astorino Teach Pendant', justification='center',font=('Cooper Black', 24), background_color='brown')],
-            # sg.Text(f'X: {self.astorino.fkine(self.astorino.q).A[0,3]} m', size=(15, 1), justification='right', key='-X-'),
-            # sg.Text(f'Y: {self.astorino.fkine(self.astorino.q).A[1,3]} m', size=(15, 1), justification='right', key='-Y-'),
-            # sg.Text(f'Z: {self.astorino.fkine(self.astorino.q).A[2,3]} m', size=(15, 1), justification='right', key='-Z-'),
-            # sg.Text(f'Rx: {np.rad2deg(self.getori()[0])} deg', size=(15, 1), justification='right', key='-RX-'),
-            # sg.Text(f'Ry: {np.rad2deg(self.getori()[1])} deg', size=(15, 1), justification='right', key='-RY-'),
-            # sg.Text(f'Rz: {np.rad2deg(self.getori()[2])} deg', size=(15, 1), justification='right', key='-RZ-'),
-            # sg.Button('E-stop', button_color=('white', 'red'), size=(30, 1), key='-ESTOP-' ),],
-
-            # [sg.Text('Joint Space Jogging', key='-JS-', font=('Cooper Black', 15), background_color='brown'), sg.Button('ENABLE CONTROLLER', key='-ENABLE-', size=(30,1), pad=(5,5))],
-            # [sg.Slider(range=(self.astorino_qlim[0, 0], self.astorino_qlim[1, 0]), default_value=np.rad2deg(self.astorino.q[0]), orientation='h',
-            #         size=self._slider_size, key='-SLIDER0-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[0])} deg', size=(10, 1), key='-BASE-', justification='right'),],
-            # [sg.Slider(range=(self.astorino_qlim[0, 1], self.astorino_qlim[1, 1]), default_value=np.rad2deg(self.astorino.q[1]), orientation='h',
-            #         size=self._slider_size, key='-SLIDER1-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[1])} deg', size=(10, 1), key='-J0-', justification='right')],
-            # [sg.Slider(range=(self.astorino_qlim[0, 2], self.astorino_qlim[1, 2]), default_value=np.rad2deg(self.astorino.q[2]), orientation='h',
-            #         size=self._slider_size, key='-SLIDER2-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[2])} deg', size=(10, 1), key='-J1-', justification='right')],
-            # [sg.Slider(range=(self.astorino_qlim[0, 3], self.astorino_qlim[1, 3]), default_value=np.rad2deg(self.astorino.q[3]), orientation='h',
-            #         size=self._slider_size, key='-SLIDER3-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[3])} deg', size=(10, 1), key='-J2-', justification='right')],
-            # [sg.Slider(range=(self.astorino_qlim[0, 4], self.astorino_qlim[1, 4]), default_value=np.rad2deg(self.astorino.q[4]), orientation='h',
-            #         size=self._slider_size, key='-SLIDER4-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[4])} deg', size=(10, 1), key='-J3-', justification='right')],
-            # [sg.Slider(range=(self.astorino_qlim[0, 5], self.astorino_qlim[1, 5]), default_value=np.rad2deg(self.astorino.q[5]), orientation='h',
-            #         size=self._slider_size, key='-SLIDER5-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[5])} deg', size=(10, 1), key='-J4-', justification='right')],
-            # [sg.Slider(range=(self.astorino_qlim[0, 6], self.astorino_qlim[1, 6]), default_value=np.rad2deg(self.astorino.q[6]), orientation='h',
-            #         size=self._slider_size, key='-SLIDER6-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[6])} deg', size=(10, 1), key='-J5-', justification='right')],
+            sg.Text(f'X: {self.astorino.fkine(self.astorino.q).A[0,3]} m', size=(15, 1), justification='right', key='-A_X-'),
+            sg.Text(f'Y: {self.astorino.fkine(self.astorino.q).A[1,3]} m', size=(15, 1), justification='right', key='-A_Y-'),
+            sg.Text(f'Z: {self.astorino.fkine(self.astorino.q).A[2,3]} m', size=(15, 1), justification='right', key='-A_Z-'),
+            sg.Text(f'Rx: {np.rad2deg(self.getori()[0])} deg', size=(15, 1), justification='right', key='-A_RX-'),
+            sg.Text(f'Ry: {np.rad2deg(self.getori()[1])} deg', size=(15, 1), justification='right', key='-A_RY-'),
+            sg.Text(f'Rz: {np.rad2deg(self.getori()[2])} deg', size=(15, 1), justification='right', key='-A_RZ-'),
+            sg.Button('E-stop', button_color=('white', 'red'), size=(30, 1), key='-ESTOP-' ),],     
+            
+            [sg.Text('Joint Space Jogging', key='-A_JS-', font=('Cooper Black', 15), background_color='brown'), sg.Button('ENABLE CONTROLLER', key='-A_ENABLE-', size=(30,1), pad=(5,5))],
+            [sg.Slider(range=(self.astorino_qlim[0, 0], self.astorino_qlim[1, 0]), default_value=np.rad2deg(self.astorino.q[0]), orientation='h',
+                    size=self._slider_size, key='-A_SLIDER0-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[0])} deg', size=(10, 1), key='-A_BASE-', justification='right'),],
+            [sg.Slider(range=(self.astorino_qlim[0, 1], self.astorino_qlim[1, 1]), default_value=np.rad2deg(self.astorino.q[1]), orientation='h',
+                    size=self._slider_size, key='-A_SLIDER1-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[1])} deg', size=(10, 1), key='-A_J0-', justification='right')],
+            [sg.Slider(range=(self.astorino_qlim[0, 2], self.astorino_qlim[1, 2]), default_value=np.rad2deg(self.astorino.q[2]), orientation='h',
+                    size=self._slider_size, key='-A_SLIDER2-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[2])} deg', size=(10, 1), key='-A_J1-', justification='right')],
+            [sg.Slider(range=(self.astorino_qlim[0, 3], self.astorino_qlim[1, 3]), default_value=np.rad2deg(self.astorino.q[3]), orientation='h',
+                    size=self._slider_size, key='-A_SLIDER3-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[3])} deg', size=(10, 1), key='-A_J2-', justification='right')],
+            [sg.Slider(range=(self.astorino_qlim[0, 4], self.astorino_qlim[1, 4]), default_value=np.rad2deg(self.astorino.q[4]), orientation='h',
+                    size=self._slider_size, key='-A_SLIDER4-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[4])} deg', size=(10, 1), key='-A_J3-', justification='right')],
+            [sg.Slider(range=(self.astorino_qlim[0, 5], self.astorino_qlim[1, 5]), default_value=np.rad2deg(self.astorino.q[5]), orientation='h',
+                    size=self._slider_size, key='-A_SLIDER5-', enable_events=True), sg.Text(f'{np.rad2deg(self.astorino.q[5])} deg', size=(10, 1), key='-A_J4-', justification='right')],
+            
         
-            # [sg.Text('TCP Jogging', key='-TCP-', font=('Cooper Black', 15), background_color='brown')],
-            # [sg.Text('X: ',    size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-CARTX-'),
-            # sg.Text('Y: ',    size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-CARTY-'),
-            # sg.Text('Z: ',    size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-CARTZ-')],
-            # [sg.Text('Roll: ', size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-ROLL-'),
-            # sg.Text('Pitch: ',size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-PITCH-'),
-            # sg.Text('Yaw: ',  size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-YAW-')],
-            # [sg.Button('+X',    key='-PLUSX-', size=self._input_size), sg.Button('+Y',        key='-PLUSY-', size=self._input_size), sg.Button('+Z', key='-PLUSZ-', size=self._input_size)],
-            # [sg.Button('-X',    key='-MINUSX-', size=self._input_size), sg.Button('-Y',       key='-MINUSY-', size=self._input_size), sg.Button('-Z', key='-MINUSZ-', size=self._input_size)],
-            # [sg.Button('+Roll', key='-PLUSROLL-', size=self._input_size), sg.Button('+Pitch', key='-PLUSPITCH-', size=self._input_size), sg.Button('+Yaw', key='-PLUSYAW-', size=self._input_size)],
-            # [sg.Button('-Roll', key='-MINUSROLL-', size=self._input_size), sg.Button('-Pitch',key='-MINUSPITCH-', size=self._input_size), sg.Button('-Yaw', key='-MINUSYAW-', size=self._input_size)],
+            [sg.Text('TCP Jogging', key='-A_TCP-', font=('Cooper Black', 15), background_color='brown')],
+            [sg.Text('X: ',    size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-A_CARTX-'),
+            sg.Text('Y: ',    size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-A_CARTY-'),
+            sg.Text('Z: ',    size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-A_CARTZ-')],
+            [sg.Text('Roll: ', size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-A_ROLL-'),
+            sg.Text('Pitch: ',size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-A_PITCH-'),
+            sg.Text('Yaw: ',  size=(5, 1), ), sg.Input(default_text='0', size=(10, 1), key='-A_YAW-')],
+            [sg.Button('+X',    key='-A_PLUSX-', size=self._input_size), sg.Button('+Y',        key='-A_PLUSY-', size=self._input_size), sg.Button('+Z', key='-A_PLUSZ-', size=self._input_size)],
+            [sg.Button('-X',    key='-A_MINUSX-', size=self._input_size), sg.Button('-Y',       key='-A_MINUSY-', size=self._input_size), sg.Button('-Z', key='-A_MINUSZ-', size=self._input_size)],
+            [sg.Button('+Roll', key='-A_PLUSROLL-', size=self._input_size), sg.Button('+Pitch', key='-A_PLUSPITCH-', size=self._input_size), sg.Button('+Yaw', key='-A_PLUSYAW-', size=self._input_size)],
+            [sg.Button('-Roll', key='-A_MINUSROLL-', size=self._input_size), sg.Button('-Pitch',key='-A_MINUSPITCH-', size=self._input_size), sg.Button('-Yaw', key='-A_MINUSYAW-', size=self._input_size)],
 
-            # [sg.Radio('Joint Space Control', 'RADIO1', default=True, key='-JOINT-', size=(18,1)),
-            # sg.Radio('End Effector Control', 'RADIO1', key='-END-EFFECTOR-', size=(18,1))],
+            [sg.Radio('Joint Space Control', 'RADIO1', default=True, key='-A_JOINT-', size=(18,1)),
+            sg.Radio('End Effector Control', 'RADIO1', key='-A_END-EFFECTOR-', size=(18,1))],
 
-            # [sg.Button('Confirm', key='-CONFIRM-', size=(18,1)), sg.Button('Home', key='-HOME-', size=(18,1)),],
+            [sg.Button('Confirm', key='-A_CONFIRM-', size=(18,1)), sg.Button('Home', key='-A_HOME-', size=(18,1)),],
             
         ]
         return tab2_layout
